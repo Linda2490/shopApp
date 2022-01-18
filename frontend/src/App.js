bd4d857e115c71a6b3c52d19aaf0f958213ef91b
Your Name <lindazoghlami@gmail.com>
2022-01-18 19:03:31 +0100

    beginning_of_the project

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import './App.css'
+
+
+function App() {
+  return (
+    <Router>
+    
+      <div>
+       
+        
+      
+      </div>
+    </Router>
+  );
+}
+
+export default App
