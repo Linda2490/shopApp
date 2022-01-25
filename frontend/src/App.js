0a5551bc615a64a2d39b4f6a47b8133b3d60f696
Your Name <lindazoghlami@gmail.com>
2022-01-25 01:15:10 +0100

    header-comp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,18 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import './App.css'
-
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <Router>
-    
-      <div>
-       
-        
-      
+      <div className="layout">
+        <Header />
+        <main>content</main>
+        <footer>footer</footer>
       </div>
     </Router>
-  );
+  )
 }
 
 export default App
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../css/Header/Header.css'
+
+export default function Header() {
+  return (<div>
+  <header>Shopping Cart</header>
+  
+  
+  </div>);
+}
