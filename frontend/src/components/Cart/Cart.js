2b991c5898758afbbde3e8dcbadd461a9270d539
Your Name <lindazoghlami@gmail.com>
2022-02-01 04:43:05 +0100

    following cart

---
+++ frontend/src/components/Cart/Cart.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../../css/Cart/Cart.css'
+export default function Cart(props) {
+  return (
+    <div className="cart-wrapper">
+      <div className="cart-title"> {props.cartItems.length === 0 ? 'empty cart' : <p>There are {props.cartItems.length} in cart </p>} </div>
+      <div className="cart-items">
+        {props.cartItems.map(item => (
+            <div className="cart-item" key={item.id}>
+          <img src={item.image} alt={item.title} />
+          <div className="cart-info">
+            <div>
+              <p>title: {item.title}</p>
+              <p>quantity: {item.qty}</p>git pull 
+              <p>price: {item.price}dt</p>
+            </div>
+
+            <button onClick={() => {props.removeProduct(item)}}>Remove</button>
+          </div>
+        </div>
+        ))}
+      </div>
+      <div className='cart-footer'>
+          <div className='total'>Total</div>
+          <button>select products</button>
+      </div>
+    </div>
+  )
+}
